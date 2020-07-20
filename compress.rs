7c49a11726f97e6612768bc4a9155adcded84ee5
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2020-07-20 10:23:50 +0100

    Add files via upload

--- compress.rs
+++ compress.rs
@@ -2,7 +2,6 @@ use crate::matcher;
 use crate::matcher::Match;
 use crate::bit::BitStream;
 use crate::block::Block;
-use crate::inflate;
 
 pub fn compress( inp: &[u8] ) -> Vec<u8>
 {
@@ -20,10 +19,9 @@ pub fn compress( inp: &[u8] ) -> Vec<u8>
   loop
   {
     let mut block_size = len - ii;
-    if block_size > 0x1000 { block_size = 0x1000; }
+    if block_size > 0x4000 { block_size = 0x4000; }
     let mut b = Block::new( ii, block_size, mi );
     b.init( &inp, &mlist );
-    // println!( "block size={}", b.input_end - ii ); 
     ii = b.input_end;
     mi = b.match_end;
     b.write( &inp, &mlist, &mut out, ii == len );
@@ -33,43 +31,6 @@ pub fn compress( inp: &[u8] ) -> Vec<u8>
   out.write( 32, adler32( &inp ) as u64 );
   out.flush();
   out.bytes
-}
-
-pub fn test( n:usize )
-{
-  // let f = std::fs::read( "C:\\PdfFiles\\FreeSans.ttf" ).unwrap();
-
-  for _i in 0..n
-  {
-    check( &[1,2,3,4], &[120,156,5,128,1,9,0,0,0,130,40,253,191,89,118,12,11,0,24,0] );
-    check( &[0,0,0,0,1,2,3,4], &[120,156,13,192,5,1,0,0,0,194,48,172,127,102,62,193,233,14,11,0,28,0] );
-    check( &[1,2,3,4,1,2,3,4,1,2,3,4,1,1,4,1,2,3,4], &[] );
-    let mut t : Vec<u8> = Vec::new();
-    for i in 0..10000 { t.push( ( ( i % 256 ) | ( i % 13 ) ) as u8 ); }
-    check( &t, &[] );
-    // check( &f, &[] );
-  }
-}
-
-pub fn check( inp: &[u8], chk: &[u8] )
-{
-  let cb : &[u8] = &compress( inp );
-
-  for i in 0..chk.len()
-  {
-    // println!( "i={} b={}", i, cb[i] );
-    // if chk[i] != cb[i] { println!( "Failed at i={}", i ); }
-    assert_eq!( chk[i], cb[i] );
-  }
-  // println!( "test ran ok inp.len={} cb.len={}", inp.len(), cb.len() );
-
-  let inf = inflate::inflate( cb );
-  for i in 0..inp.len()
-  {
-    assert_eq!( inf[i], inp[i] );
-  }
-
-
 }
 
 /// Checksum function per RFC 1950.
--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
-// use rand::Rng;
+﻿// use rand::Rng;
 use std::time::Instant;
 
 mod compress;
@@ -12,7 +12,42 @@ fn main()
 {
  let start = Instant::now();
 
- let n = 100;
- compress::test( n );
- println!( "test completed ok, n={} time elapsed={} ms.", n, start.elapsed().as_millis() );
+ let n = 10;
+ test( n );
+ println!( "test completed ok, n={} time elapsed={} micro sec.", n, start.elapsed().as_micros() );
+}
+
+pub fn test( n:usize )
+{
+  // let f = std::fs::read( "C:\\PdfFiles\\FreeSans.ttf" ).unwrap();
+
+  for _i in 0..n
+  {
+    check( &[1,2,3,4], &[120,156,5,128,1,9,0,0,0,130,40,253,191,89,118,12,11,0,24,0] );
+    check( &[0,0,0,0,1,2,3,4], &[120,156,13,192,5,1,0,0,0,194,48,172,127,102,62,193,233,14,11,0,28,0] );
+    check( &[1,2,3,4,1,2,3,4,1,2,3,4,1,1,4,1,2,3,4], &[] );
+    let mut t : Vec<u8> = Vec::new();
+    for i in 0..10000 { t.push( ( ( i % 256 ) | ( i % 13 ) ) as u8 ); }
+    check( &t, &[] );
+    // check( &f, &[] );
+  }
+}
+
+pub fn check( inp: &[u8], chk: &[u8] )
+{
+  let cb : &[u8] = &compress::compress( inp );
+
+  for i in 0..chk.len()
+  {
+    // println!( "i={} b={}", i, cb[i] );
+    // if chk[i] != cb[i] { println!( "Failed at i={}", i ); }
+    assert_eq!( chk[i], cb[i] );
+  }
+  //println!( "test ran ok inp.len={} cb.len={}", inp.len(), cb.len() );
+
+  let inf = inflate::inflate( cb );
+  for i in 0..inp.len()
+  {
+    assert_eq!( inf[i], inp[i] );
+  }
 }
