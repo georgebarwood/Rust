0b7a47aa8614171163390acdffc1efa79655ba47
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2020-07-26 21:53:57 +0100

    Add files via upload

--- compress.rs
+++ compress.rs
@@ -1,10 +1,10 @@
+use scoped_threadpool::Pool;
+use crossbeam::{channel,Receiver};
+
 use crate::matcher;
 use crate::matcher::Match;
 use crate::bit::BitStream;
 use crate::block::Block;
-
-use scoped_threadpool::Pool;
-use crossbeam::{channel,Receiver};
 
 pub fn compress( inp: &[u8], p: &mut Pool ) -> Vec<u8>
 {
@@ -15,86 +15,50 @@ pub fn compress( inp: &[u8], p: &mut Pool ) -> Vec<u8>
   p.scoped( |s| 
   {
     s.execute( || { matcher::find( inp, tx ); } );
-    do_blocks( inp, rx, &mut out );
+    write_blocks( inp, rx, &mut out );
   } );
 
   out.bytes
 }
 
-pub fn do_blocks( inp: &[u8], rx: Receiver<Match>, out: &mut BitStream )
+pub fn write_blocks( inp: &[u8], rx: Receiver<Match>, out: &mut BitStream )
 {
   out.write( 16, 0x9c78 );
 
   let len = inp.len();
-  let mut ii = 0; // input index
-  let mut mi = 0; // match index
-  let mut mp = 0; // match position
-  let mut mlist : Vec<Match> = Vec::new();
+  let mut block_start = 0; // start of next block
+  let mut match_start = 0; // start of matches for next block
+  let mut match_position = 0; // latest match position
+  let mut mlist : Vec<Match> = Vec::new(); // list of matches
   loop
   {
-    let mut block_size = len - ii;
+    let mut block_size = len - block_start;
     if block_size > 0x4000 { block_size = 0x4000; }
-    let mut b = Block::new( ii, block_size, mi );
+    let mut b = Block::new( block_start, block_size, match_start );
 
-    while mp < b.input_end // Get matches for the block.
+    while match_position < b.input_end // Get matches for the block.
     {
       match rx.recv()
       {
         Ok( m ) => 
         {
-          mp = m.position;
+          match_position = m.position;
           mlist.push( m );          
         },
-        Err( _err ) => mp = len
+        Err( _err ) => match_position = len
       }
     }
 
     b.init( &inp, &mlist );
-    ii = b.input_end;
-    mi = b.match_end;
-    b.write( &inp, &mlist, out, ii == len );
-    if ii == len { break; }
+    block_start = b.input_end;
+    match_start = b.match_end;
+    b.write( &inp, &mlist, out, block_start == len );
+    if block_start == len { break; }
   }   
   out.pad(8);
   out.write( 32, adler32( &inp ) as u64 );
   out.flush();
-
-  // println!( "Total matches={}", mlist.len() );
 }
-
-/*
-pub fn compress( inp: &[u8] ) -> Vec<u8>
-{
-  let mut out = BitStream::new();
-  out.write( 16, 0x9c78 );
-
-  let mut mlist : Vec<Match> = Vec::new();
-  matcher::find( inp, &mut mlist );
-
-  // for mat in &mlist { println!( "Match at {} length={} distance={}", mat.position, mat.length, mat.distance ); } }
-  // println!( "Total matches={}", mlist.len() );
-
-  let len = inp.len();
-  let mut ii = 0; // input index
-  let mut mi = 0; // match index
-  loop
-  {
-    let mut block_size = len - ii;
-    if block_size > 0x4000 { block_size = 0x4000; }
-    let mut b = Block::new( ii, block_size, mi );
-    b.init( &inp, &mlist );
-    ii = b.input_end;
-    mi = b.match_end;
-    b.write( &inp, &mlist, &mut out, ii == len );
-    if ii == len { break; }
-  }   
-  out.pad(8);
-  out.write( 32, adler32( &inp ) as u64 );
-  out.flush();
-
-  out.bytes
-}
-*/
 
 /// Checksum function per RFC 1950.
 pub fn adler32( input: &[u8] ) -> u32
